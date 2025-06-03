c58300f398c5e9860f0aece8e5646e00a631d811
anuj-more-zeus <anuj.more@zeuslearning.com>
2025-06-03 17:21:58 +0530

    Complete task 1

---
+++ index.js
@@ -0,0 +1,16 @@
+
+
+// function validateForm() {
+//     const name = document.getElementById('name').value
+//     const comments = document.getElementById('comments').value
+//     const genders = document.getElementsByName('sex')
+
+//     let genderSelected = false;
+
+//     for(let i=0; i<genders.length; i++){
+//         if(genders[i].checked){
+//             genderSelected = true
+//             break;
+//         }
+//     }
+// }
