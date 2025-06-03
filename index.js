029a210b26a1e8859ab170a5fef14457531466ab
anuj-more-zeus <anuj.more@zeuslearning.com>
2025-06-03 18:52:23 +0530

    Make changes to replicate the given requirements.

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
 
 
-// function validateForm() {
-//     const name = document.getElementById('name').value
-//     const comments = document.getElementById('comments').value
-//     const genders = document.getElementsByName('sex')
+document.getElementById('submit').addEventListener('submit', function(event){
+     event.preventDefault()
+})
 
-//     let genderSelected = false;
+function validateForm() {
+    const name = document.getElementById('name').value
+    const comments = document.getElementById('comments').value
+    const genders = document.getElementsByName('sex')
 
-//     for(let i=0; i<genders.length; i++){
-//         if(genders[i].checked){
-//             genderSelected = true
-//             break;
-//         }
-//     }
-// }
+    let genderSelected = false;
+
+    for(let i=0; i<genders.length; i++){
+        if(genders[i].checked){
+            genderSelected = true
+            break;
+        }
+    }
+
+    if(!name) {
+        alert('name is required')
+        return false
+    }
+
+    if(!comments) {
+        alert('comment is required')
+        return false
+    }
+
+    if(!genderSelected) {
+        alert('gender is required')
+        return false
+    }
+
+    return true
+}
